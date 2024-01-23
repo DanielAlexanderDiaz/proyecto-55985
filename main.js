ac94ef5ef331fb66bfb2f1decbbf43b1016c4c3b
Daniel Alexander <danieldiazalexander@gmail.com>
2024-01-23 15:17:50 -0300

    agregando más codigo

--- main.js
+++ main.js
@@ -3,49 +3,55 @@ alert("Bienvenido a Los Más Sabrosos")
 
 let nombrePersona = prompt("Cual es tu nombre?")
 
-alert(nombrePersona + ", Que helado te gustaria")
+alert(nombrePersona + ", que helado te gustaria")
 
-const helado1 = { id: 1,nombre: 'Helado de chocolate',precio: 1000 } 
+const helado1 = { id: 1,nombre: 'Helado de chocolate'} 
 
-const helado2 = { id: 2,nombre: 'Helado de vainilla',precio: 1000 } 
+const helado2 = { id: 2,nombre: 'Helado de vainilla'} 
 
-const helado3 = { id: 3,nombre: 'Helado de crema',precio: 900 } 
+const helado3 = { id: 3,nombre: 'Helado de crema'} 
 
 const helados = [helado1, helado2, helado3]
 
-/*function listarHelados(helados){
-    for (let i = 0; i < helados.length; i++) {
-        console.log(helados[i].id + " - " +helados[i].nombre + " - $" + helados[i].precio)
-    }
-}
-
-listarHelados(helados)*/
-
-function mostrarHelados(helados){
-    let mensaje = "Lista de helados \n"
+function listarHelados(helados){
+    let mensaje = "Lista de helados disponibles \n \n"
 
     for (let i = 0; i < helados.length; i++) {
-        mensaje += helados[i].id + " - Nombre: " +helados[i].nombre + " - Precio: $" + helados[i].precio + '\n'
+        mensaje += helados[i].id + " - " +helados[i].nombre + "\n"
     }
 
     alert(mensaje)
 }
 
-mostrarHelados(helados)
+listarHelados(helados)
 
-let opcion = prompt("Eligue una opcion")
+let opcion = prompt("Eligue una opción")
+
+function buscarHelado(array,opcion){
+    let objeto = array[opcion-1]
+    let nombre = objeto.nombre
+
+    return nombre
+}
 
 switch(opcion > 0 ){
     case opcion == 1:
-        alert("elegiste la opcion N°1")
+        alert("elegiste la opción 1: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
         break
     case opcion == 2:
-        alert("elegiste la opcion N°2")
+        alert("elegiste la opción 2: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
+        alert(buscarHelado(helados,opcion))
         break
     case opcion == 3:
-        alert("elegiste la opcion N°3")
+        alert("elegiste la opción 3: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
         break
     case opcion > 3:
-        alert('opcion no valida')
+        alert("Opción no valida")
         break
 }
+
+
+
+
+
+
