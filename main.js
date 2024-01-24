d90ac76d65249c66a0b337faa1a0f3f3d05f2f00
Daniel Alexander <danieldiazalexander@gmail.com>
2024-01-24 12:16:35 -0300

    avanzando

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-
-/*
-alert("Bienvenido a la heladeria: Los Más Sabrosos")
-
-let nombrePersona = prompt("Cual es tu nombre?")
-
-alert(nombrePersona + ", que helado te gustaria")
-
 const helado1 = { id: 1,nombre: 'Helado de chocolate'} 
 
 const helado2 = { id: 2,nombre: 'Helado de vainilla'} 
 
 const helado3 = { id: 3,nombre: 'Helado de crema'} 
 
-const helados = [helado1, helado2, helado3]
+const negacion = { id: 4,nombre: 'Salir'} 
+
+const helados = [helado1, helado2, helado3,negacion]
 
 function listarHelados(helados){
 
@@ -25,10 +19,6 @@ function listarHelados(helados){
     alert(mensaje)
 }
 
-listarHelados(helados)
-
-let opcion = prompt("Eligue una opción")
-
 function buscarHelado(array,opcion){
     
     let objeto = array[opcion-1]
@@ -37,42 +27,83 @@ function buscarHelado(array,opcion){
     return nombre
 }
 
-switch(opcion > 0 ){
-    case opcion == 1:
-        alert("elegiste la opción 1: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
-        break
-    case opcion == 2:
-        alert("elegiste la opción 2: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
-        alert(buscarHelado(helados,opcion))
-        break
-    case opcion == 3:
-        alert("elegiste la opción 3: " + buscarHelado(helados,opcion) + "\n" + "Que lo disfrutes!!")
-        break
-    case opcion > 3:
-        alert("Opción no valida")
-        break
-}
-*/
-const helado1 = { id: 1,nombre: 'Helado de chocolate'} 
-
-const helado2 = { id: 2,nombre: 'Helado de vainilla'} 
-
-const helado3 = { id: 3,nombre: 'Helado de crema'} 
-
-const helados = [helado1, helado2, helado3]
-
-
-
-
+/******************************************************************/
 
 alert("Bienvenido a la heladeria: Los Más Sabrosos")
 
-let confirmacion = prompt("si o no")
+let nombrePersona = prompt("Cual es tu nombre?")
 
-while(confirmacion=="SI"){
-    alert(nombrePersona + ", que helado te gustaria")
+alert(nombrePersona + ", Cual helado te gustaria?")
+
+listarHelados(helados)
+
+let primeraOpcion = prompt("Eligue una opción")
+
+switch(primeraOpcion > 0 ){
+
+    case primeraOpcion == 1:
+        alert("elegiste la opción 1: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
+        break
+
+    case primeraOpcion == 2:
+        alert("elegiste la opción 2: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
+        break
+
+    case primeraOpcion == 3:
+        alert("elegiste la opción 3: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
+        break
+
+    case primeraOpcion > 3:
+        alert("Adios")
+        break
+}
+
+
+let segundaOpcion;
+
+if(primeraOpcion == 1 || primeraOpcion == 2 || primeraOpcion == 3){
+
+   let otro = prompt("Quieres otro?")
+
+   if(otro == "si"){
+
+    listarHelados(helados)
+
+    segundaOpcion = prompt("Eligue una opción")
+
+        switch(segundaOpcion > 0 ){
+
+            case segundaOpcion == 1:
+                alert("elegiste la opción 1: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                break
+    
+            case segundaOpcion == 2:
+                alert("elegiste la opción 2: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                break
+    
+            case segundaOpcion == 3:
+                alert("elegiste la opción 3: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                break
+        }
+   } 
+}
+
+if(primeraOpcion > 0 && segundaOpcion > 0){
+
+    alert("Llevas: " + buscarHelado(helados,primeraOpcion) + " y " + buscarHelado(helados,segundaOpcion))
 
 }
+else if(primeraOpcion > 0 && segundaOpcion == null){
+
+    alert("Llevas solo: " + buscarHelado(helados,primeraOpcion))
+
+}else{
+
+    alert("error")
+
+}
+
+
 
 
 
