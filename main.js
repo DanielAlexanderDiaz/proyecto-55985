c0107ff6da31512303baea760a70aca7a6a7da43
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2024-01-24 23:39:30 -0300

    Avanzando en JS

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const helado1 = { id: 1,nombre: 'Helado de chocolate', stock: 10} 
+const helado1 = { id: 1,nombre: 'Helado de chocolate', precio: 1000} 
 
-const helado2 = { id: 2 , nombre: 'Helado de vainilla' , stock: 10} 
+const helado2 = { id: 2 , nombre: 'Helado de vainilla' , precio: 950} 
 
-const helado3 = { id: 3 , nombre: 'Helado de crema' , stock: 10} 
+const helado3 = { id: 3 , nombre: 'Helado de crema' , precio: 900} 
 
-const negacion = { id: 4,nombre: 'Salir', stock: 10} 
-
-const helados = [helado1, helado2, helado3,negacion]
+const helados = [helado1, helado2, helado3]
 
 function listarHelados(helados){
 
     let mensaje = "Lista de helados disponibles \n \n"
 
     for (let i = 0; i < helados.length; i++) {
-        mensaje += helados[i].id + " - " + helados[i].nombre + " -  $" + helados[i].stock + "\n"
+        mensaje += helados[i].id + " - " + helados[i].nombre + " -  $" + helados[i].precio + "\n"
     }
 
     alert(mensaje)
@@ -25,6 +23,14 @@ function buscarHelado(array,opcion){
     let nombre = objeto.nombre
 
     return nombre
+}
+
+function traerPrecio(array,opcion){
+
+    let objeto = array[opcion-1]
+    let precio = objeto.precio
+
+    return precio
 }
 
 /******************************************************************/
@@ -39,22 +45,28 @@ listarHelados(helados)
 
 let primeraOpcion = prompt("Eligue una opción")
 
-switch(primeraOpcion == 1 || primeraOpcion == 2 || primeraOpcion == 3){
+if(primeraOpcion == 1){
 
-    case primeraOpcion == 1:
-        alert("elegiste la opción 1: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
-        
-    case opcion == 2:
-        alert("elegiste la opción 2: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
-
-    case opcion == 3:
-        alert("elegiste la opción 3: " + buscarHelado(helados,primeraOpcion) + "\n" + "Que lo disfrutes!!")
-
-    case primeraOpcion > 3:
-        alert("Adios")
-        break
 }
 
+switch(primeraOpcion > 0){
+
+    case primeraOpcion == 1:
+        alert("Elegiste la opción 1:\n\n" + buscarHelado(helados,primeraOpcion))
+        break
+
+    case primeraOpcion == 2:
+        alert("Elegiste la opción 2:\n\n" + buscarHelado(helados,primeraOpcion))
+        break
+
+    case primeraOpcion == 3:
+        alert("Elegiste la opción 3:\n\n" + buscarHelado(helados,primeraOpcion))
+        break
+
+    case primeraOpcion > 3:
+        alert("Opción no valida \n Espero que vuelvas pronto, adios")
+        break
+}
 
 let segundaOpcion;
 
@@ -71,15 +83,15 @@ if(primeraOpcion == 1 || primeraOpcion == 2 || primeraOpcion == 3){
         switch(segundaOpcion > 0 ){
 
             case segundaOpcion == 1:
-                alert("elegiste la opción 1: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                alert("elegiste la opción 1: \n\n" + buscarHelado(helados,segundaOpcion))
                 break
     
             case segundaOpcion == 2:
-                alert("elegiste la opción 2: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                alert("elegiste la opción 2: \n\n" + buscarHelado(helados,segundaOpcion))
                 break
     
             case segundaOpcion == 3:
-                alert("elegiste la opción 3: " + buscarHelado(helados,segundaOpcion) + "\n" + "Que lo disfrutes!!")
+                alert("elegiste la opción 3: \n\n" + buscarHelado(helados,segundaOpcion))
                 break
         }
    } 
@@ -87,12 +99,13 @@ if(primeraOpcion == 1 || primeraOpcion == 2 || primeraOpcion == 3){
 
 if(primeraOpcion > 0 && segundaOpcion > 0){
 
-    alert("Llevas: " + buscarHelado(helados,primeraOpcion) + " y " + buscarHelado(helados,segundaOpcion))
+    alert("Llevas: \n\n" + buscarHelado(helados,primeraOpcion) + "\n" 
+    + buscarHelado(helados,segundaOpcion) + "\n\n" + "Total $" + (traerPrecio(helados, primeraOpcion) + traerPrecio(helados, segundaOpcion)) + "\n\n Que los disfrutes!!")
 
 }
 else if(primeraOpcion > 0 && segundaOpcion == null){
 
-    alert("Llevas solo: " + buscarHelado(helados,primeraOpcion))
+    alert("Llevas solo: \n\n" + buscarHelado(helados,primeraOpcion) + "\n\n" + "Total: $" + traerPrecio(helados, primeraOpcion) + "\n\n Que lo disfrutes!!")
 
 }else{
 
