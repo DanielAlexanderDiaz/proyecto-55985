ca66dee40a60d041f4a76fea08750d907b05469f
Daniel Alexander <danieldiazalexander@gmail.com>
2024-01-25 11:40:53 -0300

    avanzando

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-/*const helado1 = { id: 1,nombre: 'Helado de chocolate', stock: 10} 
-
-const helado2 = { id: 2 , nombre: 'Helado de vainilla' , precio: 950} 
-
-const helado3 = { id: 3 , nombre: 'Helado de crema' , stock: 10} */
-
-
 const helados = [
-                { id: 1,nombre: 'Helado de chocolate', precio: 10},
-                { id: 2 , nombre: 'Helado de vainilla' , precio: 10},
-                { id: 3 , nombre: 'Helado de crema' , precio: 10} 
+                { id: 1,  nombre: 'Helado de chocolate', precio: 1000},
+                { id: 2 , nombre: 'Helado de vainilla' , precio: 950},
+                { id: 3 , nombre: 'Helado de crema' , precio: 900} 
                 ]
 
-const carro = []
+const carrito = []
 
-/*function listarHelados(helados){
+/********************************************************************/
+
+function listarHelados(helados){
 
     let mensaje = "Lista de helados disponibles \n \n"
 
@@ -22,22 +17,24 @@ const carro = []
     }
 
     alert(mensaje)
-}*/
+}
 
-/*function buscarHelado(array,opcion){
+function listarCarrito(array){
+
+    for (const productoCarrito of array) {
+
+        console.log(productoCarrito)
+
+    }
+
+}
+
+function buscarHelado(id){
     
-    let objeto = array[opcion-1]
+    let objeto = helados[id-1]
     let nombre = objeto.nombre
 
     return nombre
-}*/
-
-function traerPrecio(array,opcion){
-
-    let objeto = array[opcion-1]
-    let precio = objeto.precio
-
-    return precio
 }
 
 /******************************************************************/
@@ -47,73 +44,78 @@ alert("Bienvenido a la heladeria: Los Más Sabrosos")
 let seleccion = prompt("Desea comprar algun helado? si o no")
 
 while(seleccion != "si" && seleccion != "no"){
+
     alert("Favor de ingresar si o no")
+
     seleccion = prompt("Desea comprar algun helado? si o no")
+
 }
 
-/*switch(primeraOpcion > 0 ){
-
-    case primeraOpcion == 1:
-        alert("Elegiste la opción 1:\n\n" + buscarHelado(helados,primeraOpcion))
-        break
-
-    case primeraOpcion == 2:
-        alert("Elegiste la opción 2:\n\n" + buscarHelado(helados,primeraOpcion))
-        break
-
-    case primeraOpcion == 3:
-        alert("Elegiste la opción 3:\n\n" + buscarHelado(helados,primeraOpcion))
-        break
-
-    case primeraOpcion > 3:
-        alert("Opción no valida \n Espero que vuelvas pronto, adios")
-        break
-}
-
-let segundaOpcion;
-
-if(primeraOpcion == 1 || primeraOpcion == 2 || primeraOpcion == 3){
-
-   let otro = prompt("Quieres otro?")
-
-   if(otro == "si"){
-
+if(seleccion == "si"){
+    
     listarHelados(helados)
 
-    segundaOpcion = prompt("Eligue una opción")
+}else if(seleccion == "no"){
 
-        switch(segundaOpcion > 0 ){
-
-            case segundaOpcion == 1:
-                alert("elegiste la opción 1: \n\n" + buscarHelado(helados,segundaOpcion))
-                break
-    
-            case segundaOpcion == 2:
-                alert("elegiste la opción 2: \n\n" + buscarHelado(helados,segundaOpcion))
-                break
-    
-            case segundaOpcion == 3:
-                alert("elegiste la opción 3: \n\n" + buscarHelado(helados,segundaOpcion))
-                break
-        }
-   } 
-}
-
-if(primeraOpcion > 0 && segundaOpcion > 0){
-
-    alert("Llevas: \n\n" + buscarHelado(helados,primeraOpcion) + "\n" 
-    + buscarHelado(helados,segundaOpcion) + "\n\n" + "Total $" + (traerPrecio(helados, primeraOpcion) + traerPrecio(helados, segundaOpcion)) + "\n\n Que los disfrutes!!")
+    alert("Espero que vuelvas pronto, adios")
 
 }
-else if(primeraOpcion > 0 && segundaOpcion == null){
 
-    alert("Llevas solo: \n\n" + buscarHelado(helados,primeraOpcion) + "\n\n" + "Total: $" + traerPrecio(helados, primeraOpcion) + "\n\n Que lo disfrutes!!")
+while(seleccion != 'no'){
 
-}else{
+    let productoSeleccionado = parseInt(prompt("Agregar un producto, ingresar su numero"))
 
-    alert("error")
+    let nombre
+    let precio = 0
 
-}*/
+   if(productoSeleccionado == 1 || productoSeleccionado == 2 || productoSeleccionado == 3){
+
+
+    switch(productoSeleccionado){
+
+        case 1:
+            nombre = buscarHelado(productoSeleccionado)
+            precio = 1000
+            break
+
+        case 2:
+            nombre = buscarHelado(productoSeleccionado)
+            precio = 950
+            break
+
+        case 3:
+            nombre = buscarHelado(productoSeleccionado)
+            precio = 900
+            break
+        
+        default:
+            break
+
+   }
+
+   carrito.push(nombre,precio)
+
+   } else{
+
+        alert("Ingresar algun producto de la lista")
+
+   }
+   
+   seleccion = prompt("Desea seguir comprando? si o no")
+
+   while(seleccion == 'no'){
+
+        alert("Tus productos seleccionados \n")
+
+        listarCarrito(carrito)
+
+        alert("Espero que vuelvas pronto, adios")
+
+        break
+   }
+
+}
+
 
 
 
